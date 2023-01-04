d74e75bd2736a92eaaa109fafc4840ad2b635c7a
kl-botsu <botsu@khonsulabs.com>
2023-01-04 18:58:54 +0000

    Deploying to gh-pages from @ khonsulabs/watchable@5e46bb921ac90e8ebcabc7341b57b18222cba740 🚀

---
+++ main/implementors/core/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"watchable":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"struct\" href=\"watchable/struct.Disconnected.html\" title=\"struct watchable::Disconnected\">Disconnected</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"watchable/enum.TimeoutError.html\" title=\"enum watchable::TimeoutError\">TimeoutError</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- main/source-files.js
+++ main/source-files.js
@@ -1,3 +1,4 @@
-var sourcesIndex = {};
-sourcesIndex["watchable"] = {"name":"","files":["lib.rs"]};
+var sourcesIndex = JSON.parse('{\
+"watchable":["",[],["lib.rs"]]\
+}');
 createSourceSidebar();
